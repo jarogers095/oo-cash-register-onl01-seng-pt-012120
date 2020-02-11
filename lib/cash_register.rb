62fdb0699eb506b75c6f91d1771807e318ed51d8
James Rogers <jarogers095@gmail.com>
2020-02-11 04:36:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,17 @@
 
 class CashRegister
+  @total
+  
+  def initialization(discount = 0)
+    @total = 0
+  end
+  
+  def total
+    return @total
+  end
+  
+  def add_item(title, price)
+    
+end
+  
   
